052e2b9df7fffa29797ffabf2fecb46ab3fdfe96
kiyomin0615 <moeday1010@gmail.com>
2023-04-19 00:15:44 +0900

    3. 에러 모달

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./Modal.module.css";
+
+function Modal(props) {
+  return (
+    <div>
+      <div className={classes.backdrop} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>확인</Button>
+        </footer>
+      </Card>
+    </div>
+  )
+}
+
+export default Modal;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import Modal from "../UI/Modal";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
@@ -8,6 +9,7 @@ function AddUser(props) {
   // state
   const [userName, setUserName] = useState("");
   const [userAge, setUserAge] = useState("");
+  const [error, setError] = useState();
 
   // event handler
   function addUserHandler(event) {
@@ -15,9 +17,17 @@ function AddUser(props) {
 
     // validation
     if (userName.trim().length === 0 || userAge.trim().length === 0) {
+      setError({
+        title: "유효하지 않은 입력",
+        message: "정확한 이름과 나이를 입력해주세요.",
+      });
       return;
     }
     if (+userAge < 1) {
+      setError({
+        title: "유효하지 않은 입력",
+        message: "정확한 나이를 입력해주세요(1 이상의 숫자).",
+      });
       return;
     }
 
@@ -37,26 +47,39 @@ function AddUser(props) {
     setUserAge(event.target.value);
   }
 
+  function confirmHandler() {
+    setError(null);
+  }
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">유저 이름</label>
-        <input
-          id="username"
-          type="text"
-          value={userName}
-          onChange={userNameChangeHandler}
+    <div>
+      {error && (
+        <Modal
+          title={error.title}
+          message={error.message}
+          onConfirm={confirmHandler}
         />
-        <label htmlFor="userAge">나이</label>
-        <input
-          id="userAge"
-          type="number"
-          value={userAge}
-          onChange={userAgeChangeHandler}
-        />
-        <Button type="submit">추가</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">유저 이름</label>
+          <input
+            id="username"
+            type="text"
+            value={userName}
+            onChange={userNameChangeHandler}
+          />
+          <label htmlFor="userAge">나이</label>
+          <input
+            id="userAge"
+            type="number"
+            value={userAge}
+            onChange={userAgeChangeHandler}
+          />
+          <Button type="submit">추가</Button>
+        </form>
+      </Card>
+    </div>
   );
 }
 
